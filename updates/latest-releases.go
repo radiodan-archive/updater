b6fc1abf60023f1e4f934363e8c5d5af19910683
Dan Nuttall <pixelblend@gmail.com>
2015-01-07 16:02:18 +0000

    Return an empty set of projects if unable to connect to deploy server

--- updates/latest-releases.go
+++ updates/latest-releases.go
@@ -1,68 +1,75 @@
 package updates
 
 import (
-  "log"
-  "io/ioutil"
-  "net/http"
-  "encoding/json"
-  "github.com/radiodan/updater/model"
+	"encoding/json"
+	"github.com/radiodan/updater/model"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func LatestReleasesByProject() (projects []Project) {
+	deployUrl := "http://deploy.radiodan.net"
 
-  body, _ := fetch("http://deploy.radiodan.net")
+	body, err := fetch(deployUrl)
 
-  var data interface{}
-  parseError := json.Unmarshal(body, &data)
-  if parseError != nil {
-    log.Printf("JSON parse error")
-  }
+	if err != nil {
+		log.Println("[!] Cannot connect to", deployUrl)
+		return
+	}
 
-  projects = parseJsonToProjects(data)
+	var data interface{}
+	parseError := json.Unmarshal(body, &data)
+	if parseError != nil {
+		log.Printf("JSON parse error")
+	}
 
-  return
+	projects = parseJsonToProjects(data)
+
+	return
 }
 
 // Fetch body from a URL
 func fetch(url string) (body []byte, err error) {
-  resp, err := http.Get(url)
+	resp, err := http.Get(url)
 
-  if err != nil {
-    log.Printf("HTTP Request Error")
-  }
-  defer resp.Body.Close()
+	if err != nil {
+		log.Printf("HTTP Request Error")
+		return
+	}
+	defer resp.Body.Close()
 
-  body, err = ioutil.ReadAll(resp.Body)
-  return
+	body, err = ioutil.ReadAll(resp.Body)
+	return
 }
 
 func parseJsonToProjects(data interface{}) (projects []Project) {
-  parsed := data.(map[string]interface{})
+	parsed := data.(map[string]interface{})
 
-  for projectName, contents := range parsed {
+	for projectName, contents := range parsed {
 
-    refs := contents.(map[string]interface{})
+		refs := contents.(map[string]interface{})
 
-    current := Project{}
-    current.Name = projectName
+		current := Project{}
+		current.Name = projectName
 
-    current.Refs = map[string]model.Release{}
+		current.Refs = map[string]model.Release{}
 
-    for refName, ref := range refs {
-      r := ref.(map[string]interface{})
+		for refName, ref := range refs {
+			r := ref.(map[string]interface{})
 
-      release := model.Release{}
-      release.Project = projectName
-      release.Ref     = refName
-      release.Source  = r["file"].(string)
-      release.Hash    = r["sha1"].(string)
-      release.Commit  = r["commit"].(string)
+			release := model.Release{}
+			release.Project = projectName
+			release.Ref = refName
+			release.Source = r["file"].(string)
+			release.Hash = r["sha1"].(string)
+			release.Commit = r["commit"].(string)
 
-      current.Refs[refName] = release
-    }
+			current.Refs[refName] = release
+		}
 
-    projects = append(projects, current)
-  }
+		projects = append(projects, current)
+	}
 
-  return
+	return
 }
